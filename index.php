488ed3ff1e37ab70875a0f30680e85f03463e09a
laorpaksa9000 <jaydee.laorpaksa@rocdeleijgraaf.nl>
2022-05-10 11:17:55 +0200

    fix

--- index.php
+++ index.php
@@ -10,25 +10,6 @@
 </head>
 <body>
 
-<?php
-	include_once 'dbh.php';
-
-	$voornaam = $_POST['Voornaam'];
-	$achternaam = $_POST['Achternaam'];
-	$email = $_POST['Email'];
-	$adres = $_POST['Adres'];
-	$postcode = $_POST['Postcode'];
-	$plaats = $_POST['Plaats'];
-	$telefoonnummer = $_POST['Telefoonnummer'];
-
-
-
-	$sql = "INSERT INTO customer (Voornaam, Achternaam, Email, Adres, Postcode, Plaats, Telefoonnummer) 
-			VALUES ('$voornaam', '$achternaam', '$email', '$adres', '$postcode', '$plaats', '$telefoonnummer');";
-		mysqli_query($con, $sql);
-
-	header("location: index.php?signup=success");
-?>
 
 </body>
 </html>
---
+++ registratie.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+        <title> Donkey Travel </title>
+		<meta charset=utf-8>
+		<meta name=description content="beschrijving">
+		<meta name=keywords content="trefword, trefword">
+		<link rel="stylesheet" href="styles.css">
+	
+</head>
+<body>
+
+<?php
+	include_once 'dbh.php';
+
+	$voornaam = $_POST['Voornaam'];
+	$achternaam = $_POST['Achternaam'];
+	$email = $_POST['Email'];
+	$adres = $_POST['Adres'];
+	$postcode = $_POST['Postcode'];
+	$plaats = $_POST['Plaats'];
+	$telefoonnummer = $_POST['Telefoonnummer'];
+
+
+
+	$sql = "INSERT INTO customer (Voornaam, Achternaam, Email, Adres, Postcode, Plaats, Telefoonnummer) 
+			VALUES ('$voornaam', '$achternaam', '$email', '$adres', '$postcode', '$plaats', '$telefoonnummer');";
+		mysqli_query($con, $sql);
+
+	header("location: index.php?signup=success");
+?>
+
+</body>
+</html>
