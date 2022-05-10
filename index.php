e29f6f6a07b9bed6da1dedee49aa3cd7b1435406
lars-aangenent <larsaangenent@hotmail.com>
2022-05-10 11:16:46 +0200

    registratie

--- index.php
+++ index.php
@@ -9,7 +9,27 @@
 	
 </head>
 <body>
-test
+
+<?php
+	include_once 'dbh.php';
+
+	$voornaam = $_POST['Voornaam'];
+	$achternaam = $_POST['Achternaam'];
+	$email = $_POST['Email'];
+	$adres = $_POST['Adres'];
+	$postcode = $_POST['Postcode'];
+	$plaats = $_POST['Plaats'];
+	$telefoonnummer = $_POST['Telefoonnummer'];
+
+
+
+	$sql = "INSERT INTO customer (Voornaam, Achternaam, Email, Adres, Postcode, Plaats, Telefoonnummer) 
+			VALUES ('$voornaam', '$achternaam', '$email', '$adres', '$postcode', '$plaats', '$telefoonnummer');";
+		mysqli_query($con, $sql);
+
+	header("location: index.php?signup=success");
+?>
+
 </body>
 </html>
 
