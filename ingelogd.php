065ec718d621189b3c41418d338d3bfca62b01e8
XanderGeurts <xander.geurts@rocdeleijgraaf.nl>
2022-06-09 11:02:03 +0200

    changes boekingen

--- ingelogd.php
+++ ingelogd.php
@@ -42,329 +42,39 @@
 <body>
 
 <div class="dropdown">
-  <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+  <button onclick="dropdownFunction()" class="dropbtn">Dropdown</button>
   <div id="myDropdown" class="dropdown-content">
-    <a href="#">Link 1</a>
+    <a href="boekingen.php">boekingen</a>
     <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div>
-    
-        <div class="container-fluid">
-            <form action="ingelogd.php" method="post">
+    <form action="ingelogd.php" method="post">
                  <input type="submit" value="Uitloggen" name="Annuleer">
             </form>
-        </div>
-        <br />
+  </div>
+</div>
 
-    <ul class="nav nav-tabs">
-        <li class="nav-item">
-            <a href="#Boekingen" class="nav-link active" data-bs-toggle="tab">Boekingen</a>
-        </li>
-        <li class="nav-item">
-            <a href="#Beheer" class="nav-link" data-bs-toggle="tab">Beheer</a>
-        </li>
-    </ul>
-    <div class="tab-content">
-        <div class="tab-pane fade show active" id="Boekingen">
-
-            <div class="tab">
-                <button class="tablinks" onclick="tab(event, 'Gasten')" id="defaultOpen">Gasten</button>
-                <button class="tablinks" onclick="tab(event, 'Herbergen')" id="HerbergenOpen">Herbergen</button>
-                <button class="tablinks" onclick="tab(event, 'Restaurants')" id="RestaurantsOpen">Restaurants</button>
-                <button class="tablinks" onclick="tab(event, 'Tochten')" id="TochtenOpen">Tochten</button>
-                <button class="tablinks" onclick="tab(event, 'Statussen')" id="statussenOpen">Statussen</button>
-            </div>
-
-            <div id="Gasten" class="tabcontent">
-                <h1> Gasten</h1>
-                <table class="table table-striped table-hover">
-                    <head>
-                        <tr class="bg-success">
-                            <td>
-                                Naam
-                            </td>
-                            <td>
-                                Email
-                            </td>
-                            <td>
-                                Telefoon
-                            </td>
-                        </tr>
-                    </head>
-                    <tbody>
-                        <?php echo $GastenTable;?>
-                    </tbody>    
-                </table>
-            </div>
-
-            <div id="Herbergen" class="tabcontent">
-            <h1> Herbergen</h1>
-              <table class="table table-striped table-hover">
-                    <head>
-                        <tr class="bg-success">
-                            <td>
-                                Naam
-                            </td>
-                            <td>
-                                Adres
-                            </td>
-                            <td>
-                                Email
-                            </td>
-                            <td>
-                                Telefoon
-                            </td>
-                            <td>
-                                Coördinaten
-                            </td>
-                            <td>
-                                <button type="button" class="btn btn-info" onclick="OpenSwalHerbegen()">
-                                    <i class="fa fa-plus"></i>
-                                </button>
-                            </td>
-                        </tr>
-                    </head>
-                    <tbody>
-                         <?php echo $HerbergTable;?>
-                    </tbody>    
-                </table>
-
-                <script type="text/javascript">
-                    
-    //functie die een swal opent met de form
-                    function OpenSwalHerbegen()
-                    {
-                        var title = "Nieuwe Herberg";
-                        var html = '<?php echo $varHerberg;?>';
-
-                        Swal.fire({
-                        title: "<b><h2>"+title+"</h2></b>", 
-                        html: html,  
-                        showCancelButton: false, 
-                        showConfirmButton: false,
-                        showClass: {
-                            popup: 'animate__animated animate__fadeInDown'
-                        },
-                        hideClass: {
-                            popup: 'animate__animated animate__fadeOutUp'
-                        }
-                        });
-                    }
-
-
-                </script>
-
-            </div>
-
-            <div id="Restaurants" class="tabcontent">
-            <h1> Restaurants</h1>
-              <table class="table table-striped table-hover">
-                    <head>
-                        <tr class="bg-success">
-                            <td>
-                                Naam
-                            </td>
-                            <td>
-                                Adres
-                            </td>
-                            <td>
-                                Email
-                            </td>
-                            <td>
-                                Telefoon
-                            </td>
-                            <td>
-                                Coördinaten
-                            </td>
-                            <td>
-                                 <button type="button" class="btn btn-info" onclick="OpenSwalRestaurants()">
-                                    <i class="fa fa-plus"></i>
-                                </button>
-                            </td>
-                        </tr>
-                    </head>
-                    <tbody>
-                        <?php echo $restaurantsTable;?>
-                    </tbody>    
-                </table>
-
-                <script>
-    //functie die een swal opent met de form
-                function OpenSwalRestaurants()
-                    {
-                        var title = "Nieuwe Restaurant";
-                        var html = '<?php echo $varRestaurants;?>';
-
-                        Swal.fire({
-                        title: "<b><h2>"+title+"</h2></b>", 
-                        html: html,  
-                        showCancelButton: false, 
-                        showConfirmButton: false,
-                        showClass: {
-                            popup: 'animate__animated animate__fadeInDown'
-                        },
-                        hideClass: {
-                            popup: 'animate__animated animate__fadeOutUp'
-                        }
-                        });
-                    }
-                </script>
-            </div>
-
-            <div id="Tochten" class="tabcontent">
-            <h1> Tochten</h1>
-                 <table class="table table-striped table-hover">
-                    <head>
-                        <tr class="bg-success">
-                            <td>
-                                Omschrijving
-                            </td>
-                            <td>
-                                Route Naam
-                            </td>
-                            <td>
-                                Aantal Dagen
-                            </td>
-                            <td>
-                                  <button type="button" class="btn btn-info" onclick="OpenSwalTochten()">
-                                    <i class="fa fa-plus"></i>
-                                </button>
-                            </td>
-                        </tr>
-                    </head>
-                    <tbody>
-                        <?php echo $TochtenTable;?>
-                    </tbody>    
-                </table>
-
-                <script type="text/javascript">
-                    
-    //functie die een swal opent met de form
-                    function OpenSwalTochten()
-                    {
-                        var title = "Nieuwe Tocht";
-                        var html = '<?php echo $varTocht;?>';
-
-                        Swal.fire({
-                        title: "<b><h2>"+title+"</h2></b>", 
-                        html: html,  
-                        showCancelButton: false, 
-                        showConfirmButton: false,
-                        showClass: {
-                            popup: 'animate__animated animate__fadeInDown'
-                        },
-                        hideClass: {
-                            popup: 'animate__animated animate__fadeOutUp'
-                        }
-                        });
-                    }
-
-                </script>
-            </div>
-
-            <div id="Statussen" class="tabcontent">
-            <h1> Statussen</h1>
-                <table class="table table-striped table-hover">
-                    <head>
-                        <tr class="bg-success">
-                            <td>
-                                Code
-                            </td>
-                            <td>
-                                Status
-                            </td>
-                            <td>
-                                verwijderbaar
-                            </td>
-                            <td>
-                                Pin toekennen
-                            </td>
-                            <td>
-                                 <button type="button" class="btn btn-info" onclick="OpenSwalStatussen()">
-                                    <i class="fa fa-plus"></i>
-                                </button>
-                            </td>
-                        </tr>
-                    </head>
-                    <tbody>
-                    <?php echo $StatusTable;?>
-                    </tbody>    
-                </table>
-
-                <script type="text/javascript">
-                    
-    //functie die een swal opent met de form
-                    function OpenSwalStatussen()
-                    {
-                        var title = "Nieuwe Status";
-                        var html = '<?php echo $varStatus;?>';
-
-                        Swal.fire({
-                        title: "<b><h2>"+title+"</h2></b>", 
-                        html: html,  
-                        showCancelButton: false, 
-                        showConfirmButton: false,
-                        showClass: {
-                            popup: 'animate__animated animate__fadeInDown'
-                        },
-                        hideClass: {
-                            popup: 'animate__animated animate__fadeOutUp'
-                        }
-                        });
-                    }
-
-                </script>
-            </div>
-        </div>
-
-        <div class="tab-pane fade" id="Beheer">
-
-        </div>
-    </div>
-    
-
-    
 
 
     <script>
-              //clickt op de defaultopen tab zodat je automatisch de gasten lijst open hebt
-            document.getElementById("defaultOpen").click();
 
-        
-        //functie zodat je tabs hebt op de pagina
-            function tab(evt, tabName) {
-            var i, tabcontent, tablinks;
-            tabcontent = document.getElementsByClassName("tabcontent");
-            for (i = 0; i < tabcontent.length; i++) {
-                tabcontent[i].style.display = "none";
-            }
-            tablinks = document.getElementsByClassName("tablinks");
-            for (i = 0; i < tablinks.length; i++) {
-                tablinks[i].className = tablinks[i].className.replace(" active", "");
-            }
-            document.getElementById(tabName).style.display = "block";
-            evt.currentTarget.className += " active";
-            }
             
 
-            function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
+                function dropdownFunction() {
+                  document.getElementById("myDropdown").classList.toggle("show");
+                }
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
+                // Close the dropdown menu if the user clicks outside of it
+                window.onclick = function(event) {
+                    if (!event.target.matches('.dropbtn')) {
+                        var dropdowns = document.getElementsByClassName("dropdown-content");
+                        var i;
+                        for (i = 0; i < dropdowns.length; i++) {
+                        var openDropdown = dropdowns[i];
+                        if (openDropdown.classList.contains('show')) {
+                            openDropdown.classList.remove('show');
+                        }
+                        }
+                    }
+                }
 
         </script>
 </body>
